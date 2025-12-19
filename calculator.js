bbca17ba485e293a85f68b0e25ff4536ffc75448
Noé <tonmail@example.com>
2025-12-19 10:25:51 +0100

    feat: add substraction operation

--- calculator.js
+++ calculator.js
@@ -17,8 +17,9 @@ function addition(a, b) {
 }
 
 function substraction(a, b) {
-  throw new Error("soustraction non implementee");
+  return a - b;
 }
+
 
 function multiplication(a, b) {
   throw new Error("multiplication non implementee");
